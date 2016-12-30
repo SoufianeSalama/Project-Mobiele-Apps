3a0e44b0e9856157727a0565135c853f5dab0ee4
SoufianeSalama <soufisalama@hotmail.com>
2016-12-30 23:07:15 +0100

    Bussen

--- app/src/main/java/be/ucll/project2/BusFragment.java
+++ app/src/main/java/be/ucll/project2/BusFragment.java
@@ -6,18 +6,26 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * Created by Meneer Doos on 20/11/2016.
  */
 
-public class ThirdFragment extends Fragment {
+public class BusFragment extends Fragment {
     View myView;
+
+    WebView webViewBusRealtime;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        myView= inflater.inflate(R.layout.third_layout,container, false);
+        myView= inflater.inflate(R.layout.bus_layout,container, false);
+
+        webViewBusRealtime =(WebView) myView.findViewById(R.id.webViewBusRealtime);
+        webViewBusRealtime.getSettings().setJavaScriptEnabled(true);
+        webViewBusRealtime.loadUrl("https://www.delijn.be/realtime/410147/50");
+
         return myView;
     }
 }
--- app/src/main/java/be/ucll/project2/MainActivity.java
+++ app/src/main/java/be/ucll/project2/MainActivity.java
@@ -1,10 +1,8 @@
 package be.ucll.project2;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -94,7 +92,7 @@ public class MainActivity extends AppCompatActivity
                     .commit();
         } else if (id == R.id.nav_third_layout) {
             fragmentManager.beginTransaction()
-                    .replace(R.id.content_frame, new ThirdFragment())
+                    .replace(R.id.content_frame, new BusFragment())
                     .commit();
         } else if (id == R.id.nav_share) {
 
